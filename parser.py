4d61eaf268a943fed2bba75e053c82df7b9fbc2e
Djion <alri7215@colorado.edu>
2016-11-15 10:11:24 -0700

    Update parser for ask reddit

--- parser.py
+++ parser.py
@@ -22,3 +22,11 @@ df2.to_csv("100k_Pics_100upvotes.csv")
 df3 = pd.read_sql("SELECT score, body from May2015 WHERE LENGTH(body) > 5 and LENGTH(body) < 100 AND subreddit='pics' AND score < 1 LIMIT 100000",sql_conn)
 
 df3.to_csv("100k_Pics_10downvotes.csv")
+
+df4 = pd.read_sql("SELECT score, body FROM May2015 WHERE LENGTH(body) > 5 AND LENGTH(body) < 100 AND subreddit='askreddit' AND ups > 100 LIMIT 10000",sql_conn)
+
+df4.to_csv("100k_Askreddit_100upvotes.csv")
+
+df5 = pd.read_sql("SELECT score, body from May2015 WHERE LENGTH(body) > 5 and LENGTH(body) < 100 AND subreddit='askreddit' AND score < 1 LIMIT 100000",sql_conn)
+
+df5.to_csv("100k_Askreddit_10downvotes.csv")
