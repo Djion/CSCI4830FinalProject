ef3a60a7573526a7db6e4dd7b4556edfc26134af
Djion <alri7215@colorado.edu>
2016-11-29 10:21:32 -0700

    Restructure

--- Data/scripts/format.py
+++ Data/scripts/format.py
@@ -2,8 +2,11 @@ from csv import DictReader, DictWriter
 import re
 
 def remove_urls (vTEXT):
-    vTEXT = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', ' urlhere ', vTEXT, flags=re.MULTILINE)
-    return(vTEXT)
+	try:
+		vTEXT = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', ' urlhere ', vTEXT, flags=re.MULTILINE)
+		return(vTEXT)
+	except Exception:
+		return('BYTE LIKE ERROR')
 def stringFormat(aString):
 	
 	aString = aString.replace("!","").replace("@","").replace("#","").replace("$","").replace("%","").replace("^","").replace("&","").replace("*","").replace("(","").replace(")","").replace("-","").replace("_","").replace("+","").replace("=","").replace("/","").replace("?","").replace(".","").replace(">","").replace(",","").replace("<","").replace(";","").replace(":","").replace("'","").replace("\\","").replace("|","").replace("}","").replace("]","").replace("{","").replace("[","").replace("~","").replace("`","").replace("\n","").replace('"','')
@@ -12,13 +15,35 @@ def stringFormat(aString):
 	return aString
 
 if __name__ == "__main__":
-	with open('100k_Pics_10downvotes.csv', encoding='utf-8', errors='ignore') as data:
+	'''
+	with open('../raw_data/100k_Askreddit_100upvotes.csv', encoding='utf-8', errors='ignore') as data:
 		readData = DictReader(data)
 		for row in readData:
 			row.pop('NUMBER', None)
 			row['BODY'] = remove_urls(row['BODY'])
 			row['BODY'] = stringFormat(row['BODY'])
 			print(row)
+	'''
+	lines = [x for x in DictReader(open('../raw_data/100k_Askreddit_100upvotes.csv', encoding='utf-8', errors='ignore'))]
+	#print(lines)
+	with open('../raw_data/100k_upvotes_formatted.csv', 'w') as csvfile:
+		fieldnames = ['SCORE', 'BODY']
+		writer = DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for row in lines:
+			row.pop('NUMBER', None)
+			row['BODY'] = remove_urls(row['BODY'])
+			row['BODY'] = stringFormat(row['BODY'])
+			writer.writerow(row)
 
-	
+	lines = [x for x in DictReader(open('../raw_data/100k_Askreddit_10downvotes.csv', encoding='utf-8', errors='ignore'))]
+	with open('../raw_data/100k_downvotes_formatted.csv', 'w') as csvfile:
+		fieldnames = ['SCORE', 'BODY']
+		writer = DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for row in lines:
+			row.pop('NUMBER', None)
+			row['BODY'] = remove_urls(row['BODY'])
+			row['BODY'] = stringFormat(row['BODY'])
+			writer.writerow(row)
 
